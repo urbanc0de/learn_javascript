7775db85bcee30da9cde1c3d50813198eed47980
= <=>
2020-11-15 11:40:01 +0100

    do while|While loop extension

---
+++ Control Flow/doWhileLoop/doWhile.js
@@ -0,0 +1,11 @@
+// -----DO While loop
+//(extension to while loops)
+
+
+//amikor index nem nagyobb/kisebb mint az érték de szeretnéd hogy minimum 1x lefusson a codeblock
+let index = 6;
+do{
+    console.log('value of index is ', index);
+    index++;
+}while (index < 6);
+//csináld amég index = 6, ha több ne futtasd le
